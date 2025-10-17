55cefa435381aea023e05686c9b79059c35faaaa
Dinesh Kumar <dineshpardhan24@gmail.com>
2025-10-17 23:08:07 +0530

    Add files via upload

---
+++ inner join.sql
@@ -0,0 +1,83 @@
+create database tushardb;
+use tushardb;
+
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(100),
+    Department VARCHAR(50),
+    EnrollmentYear INT
+);
+
+CREATE TABLE Courses (
+    CourseID INT PRIMARY KEY,
+    CourseName VARCHAR(100),
+    Department VARCHAR(50),
+    Credits INT
+);
+
+CREATE TABLE Enrollments (
+    EnrollmentID INT PRIMARY KEY,
+    StudentID INT,
+    CourseID INT,
+    EnrollmentDate DATE,
+    FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+    FOREIGN KEY (CourseID) REFERENCES Courses(CourseID)
+);
+
+
+
+-- Students Table
+INSERT INTO Students VALUES
+(1, 'Aditi Sharma', 'Computer Science', 2022),
+(2, 'Rohan Patel', 'Mechanical', 2023),
+(3, 'Sara Khan', 'Computer Science', 2022),
+(4, 'Michael Brown', 'Civil', 2023),
+(5, 'Neha Gupta', 'Mechanical', 2024),
+(6, 'David Miller', 'Civil', 2022),
+(7, 'Priya Verma', 'Computer Science', 2023);
+
+-- Courses Table
+INSERT INTO Courses VALUES
+(101, 'Data Structures', 'Computer Science', 4),
+(102, 'Thermodynamics', 'Mechanical', 3),
+(103, 'Fluid Mechanics', 'Civil', 4),
+(104, 'DBMS', 'Computer Science', 3),
+(105, 'Machine Design', 'Mechanical', 4),
+(106, 'Structural Analysis', 'Civil', 3),
+(107, 'Operating Systems', 'Computer Science', 4);
+
+-- Enrollments Table
+INSERT INTO Enrollments VALUES
+(1, 1, 101, '2025-01-10'),
+(2, 1, 104, '2025-01-12'),
+(3, 2, 102, '2025-01-11'),
+(4, 2, 105, '2025-01-15'),
+(5, 3, 101, '2025-01-13'),
+(6, 3, 107, '2025-01-17'),
+(7, 4, 103, '2025-01-16'),
+(8, 5, 102, '2025-01-18'),
+(9, 6, 103, '2025-01-19'),
+(10, 7, 104, '2025-01-20'),
+(11, 7, 107, '2025-01-21');
+select * from Students;
+
+
+-- 1. Count how many students are in each department.
+
+select department,count(studentid) as TotalStudents 
+from students group by department 
+order by department;
+
+-- 2. Find how many courses each department offers.
+select department,count(courseid) as TotalCourses
+ from courses group by department
+ order by department;
+ 
+ -- 3 List students with the courses they are enrolled in...
+    
+    SELECT s.StudentName,c.CourseName,c.Department,e.EnrollmentDate
+FROM Students s
+JOIN Enrollments e ON s.StudentID = e.StudentID
+JOIN Courses c ON e.CourseID = c.CourseID
+ORDER BY s.StudentName;
+
